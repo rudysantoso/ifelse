d2ee8d33712e21d70c617c9b286c45bdeb7587f5
rudysantoso <rudysantoso1702@gmail.com>
2020-04-22 06:55:27 +0700

    perubahan

--- ifelse.js
+++ ifelse.js
@@ -1,13 +1,18 @@
-var nama = 'mikael'
-var peran = ''
-if (!nama == '' && peran == ''){
-    console.log('Nama harus diisi!')
-} else if(nama == 'mikael' && peran == ''){
-    console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`)
-} else if(peran === 'Ksatria'){
-    var tugas = "kamu dapat menyerang dengan senjatamu!";
-    var nama = 'Nana'
-    var peran ='Ksatria'
-    console.log(`Selamat datang di Dunia Proxytia,${nama}`);
-    console.log(`Halo ${peran} ${nama}, ${tugas}`)
+
+let nama = 'Zero';
+let peran = 'Penyihir';
+
+if (nama === '' && peran === '' ) {
+    console.log('Nama harus diisi!');
+} else if (nama && peran === '') {
+    console.log('halo ' + nama + ',pilih peranmu untuk memulai game!')
+} else if (nama && peran === 'Kesatria') {
+    console.log('Selamat datang di Dunia Proxitia,' + nama)
+    console.log('Halo, ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!')
+} else if (nama && peran === 'Tabib') {
+    console.log('Selamat datang di Proxitia,' + nama)
+    console.log('Halo, ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka!')
+} else if (nama && peran === 'Penyihir') {
+    console.log('Selamat datang di Proxitia,' + nama)
+    console.log('Halo, ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!')
 }
